f8907d6343e0791f987164215fa603c43d6e76f8
iop2589 <qkrtjdcjf07@naver.com>
2023-06-05 12:48:49 +0900

    first commit

---
+++ JavaLearn/src/chap_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _Quiz_01 {
+  public static void main(String[] args) {
+    String busNo = "중랑01";
+    int timeRemainingMinute = 5;
+    double distanceRemaining = 5.5;
+
+    System.out.println(busNo + "번 버스");
+    System.out.println("약 " + timeRemainingMinute + "분 후 도착");
+    System.out.println("남은거리 " + distanceRemaining + "km");
+  }
+}
